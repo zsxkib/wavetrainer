321a2f0692ac5c88f9a8699e74822e83b9c64608
Will Sackfield <will.sackfield@gmail.com>
2025-04-28 18:12:53 -0400

    Allow correlation reducer to choose its threshold

--- wavetrainer/reducer/correlation_reducer.py
+++ wavetrainer/reducer/correlation_reducer.py
@@ -1,18 +1,22 @@
 """A reducer that removes correlation features."""
 
+import optuna
+import pandas as pd
 from feature_engine.selection import DropCorrelatedFeatures
 
 from .base_selector_reducer import BaseSelectorReducer
 
 _CORRELATION_REDUCER_FILENAME = "correlation_reducer.joblib"
+_CORRELATION_REDUCER_THRESHOLD = "correlation_reducer_threshold"
 
 
 class CorrelationReducer(BaseSelectorReducer):
     """A class that removes correlated values from a dataset."""
 
     def __init__(self) -> None:
+        self._correlation_selector = DropCorrelatedFeatures(missing_values="ignore")
         super().__init__(
-            DropCorrelatedFeatures(missing_values="ignore"),
+            self._correlation_selector,
             _CORRELATION_REDUCER_FILENAME,
         )
 
@@ -23,3 +27,10 @@ class CorrelationReducer(BaseSelectorReducer):
     @classmethod
     def should_raise(cls) -> bool:
         return False
+
+    def set_options(
+        self, trial: optuna.Trial | optuna.trial.FrozenTrial, df: pd.DataFrame
+    ) -> None:
+        self._correlation_selector.threshold = trial.suggest_float(
+            _CORRELATION_REDUCER_THRESHOLD, 0.1, 0.9
+        )
